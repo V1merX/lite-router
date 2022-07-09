072f705d3c7f76bd528f5c40af054886c4f85918
Vimer <95643848+V1merX@users.noreply.github.com>
2022-07-09 21:32:13 +0300

    Update index.php
    Added instructions for connecting adding pages to the database.

--- index.php
+++ index.php
@@ -5,3 +5,14 @@ $url = key($_GET);
 $r = new Router();
 
 $r->addRoute("/test","test.php");
+
+/*
+You can add pages through the database. To do this, create a connection and use this code:
+
+$db = new PDO ('mysql:host=ip;dbname=database_name', 'db_user', 'db_password');
+$res = $db->query('SELECT page_name, page_file FROM pages');
+while ($i = $res->fetch()) {
+ $r->addRoute("$i[page_name]","$i[page_file]");
+}
+
+*/
